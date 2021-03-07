c2b014630d1dcfb634d48c8826987579071f63a0
NBO2001 <natanbezerr63@gmail.com>
2021-03-06 22:25:58 -0400

    Modify names

--- src/main/java/view/Home.java
+++ src/main/java/view/Home.java
@@ -1,15 +1,10 @@
 package view;
 
 import controller.ControllerSelect;
-import model.Conect;
 
 import java.sql.Connection;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+
 
 public class Home {
 
